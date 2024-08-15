f22805cae9b8450df70d41e4f7f449d786cb83cc
Marcello Magno <marcello_magno@hotmail.com>
2024-08-15 12:07:24 -0300

    Update payments route

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from repository.database import db
 from db_models.payment import Payment
-
+from datetime import datetime, timedelta
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'SECRET_KEY_WEBSOCKET'
@@ -11,7 +11,20 @@ db.init_app(app)
 #Route responsible for creating the payment.
 @app.route('/payments/pix', methods={'POST'})
 def create_payments_pix():
-    return jsonify({"message": "the pyment has been created"})
+    data = request.get_json()
+    
+    if 'value' not in data:
+        return jsonify("message": "Invalid value"), 400
+    
+    expiration_date = datetime.new() + timedelta(minutes=30)
+
+    new_payment = Payment(value=data['value'],
+                          expiration_date=expiration_date)
+    db.session.add(new_payment)
+    db.session.commit()
+
+    return jsonify({"message": "the pyment has been created",
+                    "payment": new_payment.to_dict()})
 
 #Route responsible for receiving the payment confirmation. "WebHook"
 @app.route('/payments/pix/confirmation', methods={'POST'})
